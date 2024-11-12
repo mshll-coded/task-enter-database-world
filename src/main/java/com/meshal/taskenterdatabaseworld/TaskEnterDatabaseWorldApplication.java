8c6a1ae9e6624c22e8f6d749ba35bfb7efa60630
meshal <meshal7@pdx.edu>
2024-11-12 11:03:46 +0300

    Initial commit

---
+++ src/main/java/com/meshal/taskenterdatabaseworld/TaskEnterDatabaseWorldApplication.java
@@ -0,0 +1,13 @@
+package com.meshal.taskenterdatabaseworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskEnterDatabaseWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskEnterDatabaseWorldApplication.class, args);
+    }
+
+}
